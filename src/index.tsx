433ded172449f019472800b3a75c708f4f94d5c8
Vit Gordon <rocket.mind@gmail.com>
2020-12-07 08:42:08 +0300

    packages fix

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import 'panic-overlay'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+import './common/reset.css'
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
