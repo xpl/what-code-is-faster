cdf50e6a83681ec5919b29b063ada277d1b2de9e
Vitalik Gordon <v-gordon@yandex-team.ru>
2020-12-07 08:51:34 +0300

    clean state

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'panic-overlay'
-
-import React from 'react'
-import { render } from 'react-dom'
-
-import './common/reset.css'
-import { App } from './App'
-
-render(<App />, document.getElementById('root'))
